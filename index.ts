4aef204798093b61057962668a42e8bf98ab5e50
UMASHIBA <umashiba1@gmail.com>
2020-05-07 20:31:33 +0900

    [SET]仮想DOMを作るのに必要な諸々のセッティングを完了

---
+++ index.ts
@@ -0,0 +1,37 @@
+import { h, render } from "./virtualDom";
+
+interface HTMLElementEvent<T extends HTMLElement> extends Event {
+  target: T;
+}
+
+const setState = (state: string) => {
+  const node = document.getElementById("app-container");
+  //   const createKeyList = () => {
+  //     return state.split(" ").map((value: string) => {
+  //       return h("div", { key: value }, [`key: ${value}`]);
+  //     });
+  //   };
+
+  if (node !== null) {
+    render(
+      node,
+      h("div", {}, [
+        h("h1", {}, [state]), //タダの文字を表したい場合はh関数のchildrenに文字のみ渡す
+        h(
+          "input",
+          {
+            type: "text",
+            value: state,
+            oninput: (e: HTMLElementEvent<HTMLInputElement>) =>
+              setState(e.target.value),
+            autofocus: true,
+          },
+          []
+        ),
+        // h("div", { id: "container" }, createKeyList()),
+      ])
+    );
+  }
+};
+
+setState("Hey");
